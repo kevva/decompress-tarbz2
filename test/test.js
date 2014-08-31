ea5c79423866d95283f34eeeab11b8949c3f2280
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-31 16:20:58 +0200

    Use ava for testing

--- test/test.js
+++ test/test.js
@@ -1,43 +1,50 @@
-/*global afterEach, describe, it */
 'use strict';
 
-var assert = require('assert');
 var Decompress = require('decompress');
-var fs = require('fs');
+var exists = require('fs').exists;
 var path = require('path');
-var rimraf = require('rimraf');
-var tarbz2 = require('../');
+var rm = require('rimraf');
+var tarBz2 = require('../');
+var test = require('ava');
 
-describe('tar()', function () {
-    afterEach(function (cb) {
-        rimraf(path.join(__dirname, 'tmp'), cb);
-    });
+test('decompress a TAR.BZ2 file', function (t) {
+    t.plan(3);
 
-    it('should decompress a TAR.BZ2 file', function (cb) {
-        var decompress = new Decompress();
+    var decompress = new Decompress()
+        .src(path.join(__dirname, 'fixtures/test.tar.bz2'))
+        .dest(path.join(__dirname, 'tmp'))
+        .use(tarBz2());
 
-        decompress
-            .src(path.join(__dirname, 'fixtures/test.tar.bz2'))
-            .dest(path.join(__dirname, 'tmp'))
-            .use(tarbz2())
-            .decompress(function (err) {
-                assert(!err);
-                assert(fs.existsSync(path.join(__dirname, 'tmp/test.jpg')));
-                cb();
+    decompress.decompress(function (err) {
+        t.assert(!err);
+
+        exists(path.join(decompress.dest(), 'test.jpg'), function (exist) {
+            t.assert(exist);
+
+            rm(decompress.dest(), function (err) {
+                t.assert(!err);
             });
+        });
     });
+});
 
-    it('should strip path level using the `strip` option', function (cb) {
-        var decompress = new Decompress();
+test('strip path level using the `strip` option', function (t) {
+    t.plan(3);
 
-        decompress
-            .src(path.join(__dirname, 'fixtures/test-nested.tar.bz2'))
-            .dest(path.join(__dirname, 'tmp'))
-            .use(tarbz2({ strip: 1 }))
-            .decompress(function (err) {
-                assert(!err);
-                assert(fs.existsSync(path.join(__dirname, 'tmp/test/test.jpg')));
-                cb();
+    var decompress = new Decompress()
+        .src(path.join(__dirname, 'fixtures/test-nested.tar.bz2'))
+        .dest(path.join(__dirname, 'tmp'))
+        .use(tarBz2({ strip: 1 }));
+
+    decompress.decompress(function (err) {
+        t.assert(!err);
+
+        exists(path.join(decompress.dest(), 'test.jpg'), function (exist) {
+            t.assert(exist);
+
+            rm(decompress.dest(), function (err) {
+                t.assert(!err);
             });
+        });
     });
 });
