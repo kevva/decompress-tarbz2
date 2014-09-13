585377acf42db4f0fdeed6acfda413795f01a1c0
Kevin Martensson <kevinmartensson@gmail.com>
2014-09-13 22:33:35 +0200

    Use vinyl-file for testing

--- test/test.js
+++ test/test.js
@@ -1,22 +1,18 @@
 'use strict';
 
-var File = require('vinyl');
-var fs = require('fs');
 var isJpg = require('is-jpg');
 var path = require('path');
+var read = require('vinyl-file').read;
 var tarbz2 = require('../');
 var test = require('ava');
 
 test('decompress a TAR.BZ2 file', function (t) {
     t.plan(2);
 
-    fs.readFile(path.join(__dirname, 'fixtures/test.tar.bz2'), function (err, buf) {
+    read(path.join(__dirname, 'fixtures/test.tar.bz2'), function (err, file) {
         t.assert(!err);
 
         var stream = tarbz2();
-        var file = new File({
-            contents: buf
-        });
 
         stream.on('data', function (file) {
             t.assert(isJpg(file.contents));
@@ -29,13 +25,10 @@ test('decompress a TAR.BZ2 file', function (t) {
 test('strip path level using the `strip` option', function (t) {
     t.plan(3);
 
-    fs.readFile(path.join(__dirname, 'fixtures/test-nested.tar.bz2'), function (err, buf) {
+    read(path.join(__dirname, 'fixtures/test-nested.tar.bz2'), function (err, file) {
         t.assert(!err);
 
         var stream = tarbz2({ strip: 1 });
-        var file = new File({
-            contents: buf
-        });
 
         stream.on('data', function (file) {
             t.assert(file.path === 'test.jpg');
